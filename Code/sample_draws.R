aeea91c47bded9b46031d8037b79b294c0c4078d
banvathmayurkumar <56780399+banvathmayurkumar@users.noreply.github.com>
2020-11-26 17:46:08 -0500

    Update sample_draws.R

--- Code/sample_draws.R
+++ Code/sample_draws.R
@@ -8,3 +8,4 @@ header <- unlist(strsplit(readLines(con,n=1), ","))
 d <- fread("shuf -n 15000000 ProjectTrainingData.csv", data.table=F)
 colnames(d) <- header
 write_csv(d[,-1], "training1.csv")
+#Samples drawn
